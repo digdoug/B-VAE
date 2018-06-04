0f7aa6d24fec75a788e8ca5594ee319588db187c
Lucas <lcsdgpinto@gmail.com>
2018-06-04 12:33:52 -0700

    Add files via upload

--- data-processing/8frame_channel_processor.py
+++ data-processing/8frame_channel_processor.py
@@ -20,18 +20,27 @@ import random
 data_dir = '/srv/nas/mk1/users/lgendele/Amanda_Screen/'
 dump_dir = '/srv/home/lpinto/data/'
 # specify the size of 
-sample_size = 4
+sample_size = 1000
+frame_size = 8
+#train_test_split = 0.5
 
 df_ROI = pd.read_csv('/srv/home/lpinto/data-processing/Amanda_all_A_rois.csv')
 # exclude the sauronx drugs
 legacy_plates = df_ROI[~df_ROI['project'].str.contains('sauronx')]['run'].unique().tolist()
 #split train test here
-plates_wanted = [str(x) for x in legacy_plates]
+random.shuffle(legacy_plates)
+train_plates = legacy_plates[0:len(legacy_plates)/2]
+test_plates = legacy_plates[len(legacy_plates)/2:len(legacy_plates)]
+train_plates = [str(x) for x in train_plates]
+test_plates = [str(x) for x in test_plates]
+print("Legacy plates: " + str(len(legacy_plates)))
+print("training size: " + str(len(train_plates)))
+print("testing size: " + str(len(test_plates)))
 assays_wanted = ["background","softTap1","ASR1","ASRtrain","VSR1","VSR2","VSR3","VSR4","ASRi","solidRed","VSRASR"]
-frame_size = 8
-def make_8_frame_to_hdf5():
+
+def make_8_frame_to_hdf5(plates_wanted,learning_tag):
 	iter_count = 0
-	h5f = h5py.File(dump_dir + 'random.hdf5','w')
+	h5f = h5py.File(dump_dir + learning_tag  + '.hdf5','w')
 
 	while iter_count < sample_size:
 		plate = str(random.sample(plates_wanted,1))[2:-2]
@@ -47,11 +56,9 @@ def make_8_frame_to_hdf5():
 		assay_tag = plate_tag+'/'+assay
 		jpgs_to_sample = os.listdir(assay_tag)				
 		eight_jpgs = []
-		# 8 is fairly arbitrary at this point, it just happens to be a timescale we think should capture most 
-		# fish movement. Susceptible to change.
-		# starting image of the 8 frames
+
 		start_idx = random.randint(0,len(jpgs_to_sample)-1-frame_size)
-		#start_idx = jpgs_to_sample.index(jpgs_to_sample[start_idx])
+
 		for x in range(start_idx,start_idx+frame_size): eight_jpgs.append(assay_tag+'/'+jpgs_to_sample[x])
 		# making huge np array here
 		eight_frame_chunk = np.array([np.array(Image.open(fname)) for fname in eight_jpgs]) 
@@ -59,7 +66,7 @@ def make_8_frame_to_hdf5():
 				
 		well = random.sample(single_plate['well_index'].unique(),1)
 		chosen_well = single_plate[single_plate['well_index'] == well]
-		print(chosen_well)
+		#print(chosen_well)
 		x1 = chosen_well['x0'].values[0]
 		x2 = chosen_well['x1'].values[0]
 		y1 = chosen_well['y0'].values[0]
@@ -67,16 +74,17 @@ def make_8_frame_to_hdf5():
 		drug = chosen_well['key'].values[0]
 		# np array indexed in (z,y,x) fashion
 		print("size of numpy: " + str(eight_frame_chunk.shape))
-		eight_frame_chunk_well = eight_frame_chunk[:,y1:y2,x1:x2]
+		
+		eight_frame_chunk_well = eight_frame_chunk[:,min(y1,y2):max(y1,y2),min(x1,x2):max(x1,x2)]
 					
 
 		jpeg_names = ' '.join(eight_jpgs)
 		jpeg_names = jpeg_names.replace("/","_")
-		save_tag = plate + "_" + str(drug) + "_well_" + str(well)
-		print(save_tag)
-		print(type(save_tag))
-		print(eight_frame_chunk_well)
-		print(type(eight_frame_chunk_well))
+		save_tag = plate + "_" + assay + '_' + str(drug) + "_well_" + str(jpgs_to_sample[start_idx])
+		#print(save_tag)
+		#print(type(save_tag))
+		#print(eight_frame_chunk_well)
+		#print(type(eight_frame_chunk_well))
 		name = "name_" + str(well)
 		h5f.create_dataset(save_tag,data=eight_frame_chunk_well,dtype='i8')
 		iter_count +=1
@@ -92,7 +100,8 @@ def main():
     signal.signal(signal.SIGINT, original_sigint_handler)
     try:
         #results = pool.map_async(make_8_frame_to_hdf5,2)
-        make_8_frame_to_hdf5()
+        make_8_frame_to_hdf5(train_plates,"training_set")
+	make_8_frame_to_hdf5(test_plates,"validation_set")
 	print("Waiting for results")
         #results.get(60000000000) # Without the timeout this blocking call ignores all signals.
     except KeyboardInterrupt:
